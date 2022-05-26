d02382569da0a2b17793274faa5a23b395ba1403
robertjahn <rob.jahn@gmail.com>
2022-05-26 13:13:17 -0400

    adjust artifact property script

--- scripts/updateArtifactProperties.sh
+++ scripts/updateArtifactProperties.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
+# This script adds artifact properties by calling the JFrog API
 # https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API#ArtifactoryRESTAPI-SetItemProperties
-
-# This script a pipeline 'demo' script that adds JFrog chart properties by calling the JFrog API
-# These can be adjusted, but these are the properties added
-# deployedOnCluster
-# deploymentTimestamp
 
 JFROG_BASE_URL=$1       # https://dtdemos.jfrog.io
 JFROG_API_KEY=$2
 JFROG_REPO=$3           # art-docker-dynatrace
 JFROG_ARTIFACT=$4       # /demoapp/0.1.6 or demoapp-0.1.0.tgz
 JFROG_PROPERTIES=$5     # prop1=value1|prop2=value2|prop3=value3
-                        # sepererate with a pipe character
+                        # seperrated with a pipe character
 
 TIME=$(date +%s)
 JFROG_ARTIFACT_URL=${JFROG_BASE_URL}/api/storage/${JFROG_REPO}/${JFROG_ARTIFACT}?properties=${JFROG_PROPERTIES}
-# API expectes %7C for | character
+# API expects %7C for | character - so use sed to search and replace
 JFROG_ARTIFACT_URL=$(echo $JFROG_ARTIFACT_URL | sed "s/|/\%7C/g")
 echo "================================================================="
-echo "Updating Helm Chart Properties"
+echo "Updating Artifact Properties"
 echo "JFROG_REPO       = $JFROG_REPO "
 echo "JFROG_ARTIFACT   = $JFROG_ARTIFACT"
 echo "JFROG_PROPERTIES = $JFROG_PROPERTIES"
